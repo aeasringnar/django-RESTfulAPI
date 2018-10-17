d724b06a97b9ce2da0639235d18242c1155279e0
aeasringnar <aeasringnar@163.com>
2018-10-17 23:11:06 +0800

    完善

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,6 @@ from base import views
 
 urlpatterns = [
     url(r'^login',views.Login.as_view(),name='login'),
-    # url(r'^register',views.Register.as_view(),name='register'),
-    # url(r'^userinfo',views.UserInfo.as_view(),name='userinfo'),
+    url(r'^register',views.Register.as_view(),name='register'),
+    url(r'^userinfo',views.UserInfo.as_view(),name='userinfo'),
 ]
--- base/views.py
+++ base/views.py
@@ -24,7 +24,7 @@ class LoginInfoSerializer(serializers.Serializer):
 class Login(generics.GenericAPIView):
     serializer_class = LoginInfoSerializer
     def post(self,request):
-        # print(request.data)
+        print(request.data)
         try:
             serializer = self.get_serializer(data=request.data)
             if not serializer.is_valid():
@@ -59,6 +59,99 @@ class Login(generics.GenericAPIView):
                 return Response({"message": "登录成功>_<", "errorCode": 0, "data": {'token':token}})
             else:
                 return Response({"message": "密码错误>_<", "errorCode": 0, "data": {}})
+        except Exception as e:
+            print(e)
+            return Response({"message": "未知错误>_<", "errorCode": 1, "data": {}})
+
+class UserRegisterSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+    captcha = serializers.CharField()
+
+class Register(generics.GenericAPIView):
+    serializer_class = UserRegisterSerializer
+    def post(self,request):
+        try:
+            print(request.data)
+            serializer = self.get_serializer(data=request.data)
+            if not serializer.is_valid():
+                return Response({"message": str(serializer.errors), "errorCode": 4, "data": {}})
+            data = (serializer.data)
+            username = data.get('username')
+            password = data.get('password')
+            captcha = data.get('captcha')
+            if username.find('@') == -1 or username.find('.') == -1:
+                phone = username
+                email = None
+            else:
+                email = username
+                phone = None
+            phone_re = re.compile(r'^1(3[0-9]|4[57]|5[0-35-9]|7[0135678]|8[0-9])\d{8}$', re.IGNORECASE)
+            email_re = re.compile(r'^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z0-9]{2,6}$', re.IGNORECASE)
+            if phone:
+                if not phone_re.match(phone):
+                    return Response({"message": "手机号格式错误>_<", "errorCode": 2, "data": {}})
+                # 搜索缓存
+                # check_captcha = cache.get("drf:captcha:" + phone)
+                print(bool(captcha))
+                #  or google_otp(captcha)
+                if bool(captcha):
+                    # or google_otp(captcha)
+                    if captcha == '123456':
+                        account_check = User.objects.filter(phone=phone, is_delete=False)
+                        if account_check:
+                            return Response({"message": "用户已经存在>_<", "errorCode": 2})
+                        account = User()
+                        account.phone = phone
+                        # account.password = create_password(password)
+                        # 明文密码
+                        account.password = password
+                        # account.birthday = datetime.date.today()
+                        account.name = phone + '手机用户'
+                        # account.group_id = 3
+                        account.save()
+                        return Response({"message": "ok", "errorCode": 0})
+                    else:
+                        return Response({"message": "验证码错误>_<", "errorCode": 2})
+                else:
+                    return Response({"message": "验证码已过期>_<", "errorCode": 2})
+            if email:
+                if not email_re.match(email):
+                    return Response({"message": "邮箱格式错误>_<", "errorCode": 2, "data": {}})
+                if not captcha:
+                    return Response({"message": "验证码已过期>_<", "errorCode": 2})
+                if '123456' != captcha:
+                    return Response({"message": "验证码错误>_<", "errorCode": 2})
+                account_check = User.objects.filter(email=email, is_delete=False)
+                if account_check:
+                    return Response({"message": "用户已经存在>_<", "errorCode": 2})
+                account = User()
+                account.email = email
+                # account.password = create_password(password)
+                # 明文密码
+                account.password = password
+                account.name = email + '邮箱用户'
+                # account.group_id = 3
+                account.save()
+                return Response({"message": "ok", "errorCode": 0})
+        except Exception as e:
+            print(e)
+            return Response({"message": "未知错误>_<", "errorCode": 1, "data": {}})
+
+
+class UserInfo(APIView):
+    # 加上用户验证 携带正确token时就会有user，否则就是AnonymousUser 就是没有用户的状态
+    authentication_classes = (JWTAuthentication,)
+    def get(self,request):
+        try:
+            # print(request.data)
+            if not request.auth:
+                return Response({"message": "请先登录>_<", "errorCode": 2, "data": {}})
+            user = User.objects.filter(id=request.user.id,is_delete=False).first()
+            serializer_user_data = UserSerializer(user)
+            json_data = {"message": "ok", "errorCode": 0, "data": {}}
+            json_data['data'] = serializer_user_data.data
+            return Response(json_data)
         except Exception as e:
             print(e)
             return Response({"message": "未知错误>_<", "errorCode": 1, "data": {}})
