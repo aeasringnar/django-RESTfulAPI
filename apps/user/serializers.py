5af96667b64ab91c71021694ec1f8dcdee2b0d02
aeasringnar <aeasringnar@163.com>
2019-08-10 18:20:31 +0800

    更新serializers字段

--- apps/user/serializers.py
+++ apps/user/serializers.py
@@ -34,7 +34,7 @@ class UserInfoSerializer(serializers.ModelSerializer, BaseModelSerializer):
 class LoginViewSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
-
+    # test = serializers.DictField(child=serializers.CharField(required=True))
 
 class AddUserSerializer(serializers.ModelSerializer, BaseModelSerializer):
 
