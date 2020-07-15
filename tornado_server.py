a54f806a1e6c759f0399098d7e07e378e7e7b7bc
aeasringanar <aeasringnar@163.com>
2020-07-15 22:52:43 +0800

    更新tornado-server

--- tornado_server.py
+++ tornado_server.py
@@ -8,6 +8,7 @@ import tornado.ioloop
 import tornado.web
 import tornado.wsgi
 from multiprocessing import cpu_count
+from django.conf import settings
 
 
 def signal_handler(signal,frame):
@@ -15,26 +16,35 @@ def signal_handler(signal,frame):
     sys.exit()
 
 
-app_path = os.path.abspath(
-    os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)
-)
-sys.path.append(os.path.join(app_path, "base_django_api"))
-
-
-define('port', default=6000, type=int, help='run server')
-
 def main():
-    tornado.options.parse_command_line()
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base_django_api.settings')
-    wsgi_app = tornado.wsgi.WSGIContainer(get_wsgi_application())
-    http_server = tornado.httpserver.HTTPServer(wsgi_app, xheaders=True)
-    http_server.listen(options.port)
-    http_server.start(cpu_count())
-    print("""[django-tornado-server]Wellcome...
+    try:
+        if len(sys.argv) < 2:
+            raise ValueError('参数异常，请尝试python tornado_server.py runserver 8080')
+        elif sys.argv[1] == 'runserver':
+            if len(sys.argv) != 3:
+                raise ValueError('runserver选项参数错误！')
+            if ':' in sys.argv[2]:
+                host, port = sys.argv[2].split(':')
+            else:
+                port = sys.argv[2]
+                host = '127.0.0.1'
+            os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base_django_api.settings')
+            wsgi_app = tornado.wsgi.WSGIContainer(get_wsgi_application())
+            server = tornado.httpserver.HTTPServer(wsgi_app, xheaders=True)
+            server.listen(int(port), host)
+            if not settings.DEBUG:
+                server.start(cpu_count())
+            print("""[django-tornado-server]Wellcome...
 Starting development server at http://%s:%s/       
-Quit the server with CTRL+C.""" % ('127.0.0.1', str(options.port)))
-    tornado.ioloop.IOLoop.instance().start()
+Quit the server with CTRL+C.""" % (host, str(port)))
+            tornado.ioloop.IOLoop.instance().start()
+        else:
+            raise ValueError('参数异常！')
+    except Exception as e:
+        print('发生异常：%s' % str(e))
 
 if __name__ == '__main__':
+    app_path = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(os.path.join(app_path, "base_django_api"))
     signal.signal(signal.SIGINT,signal_handler)
     main()
