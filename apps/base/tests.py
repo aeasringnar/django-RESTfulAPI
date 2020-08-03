4e004ab3ab72abdd16d37bbb305badcd61a4857d
aeasringanar <aeasringnar@163.com>
2020-08-03 22:03:50 +0800

    增加测试用例

--- apps/base/tests.py
+++ apps/base/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.test import Client
+import unittest
 
 # Create your tests here.
+class TestSubtract(TestCase):
+
+    def test_public_test(self):
+        """
+        未传参数
+        :return:
+        """
+        res = self.client.get('/test/')
+        status_code = res.status_code
+        res_data = res.json()
+        self.assertEqual(status_code, 200)
+        self.assertEqual(res_data.get('errorCode'), 0)
+
+    def test_public_test_params(self):
+        """
+        传入相关参数
+        :return:
+        """
+        res = self.client.get('/test/', {'page': 1, 'page_size': 10})
+        status_code = res.status_code
+        res_data = res.json()
+        self.assertEqual(status_code, 200)
+        self.assertEqual(res_data.get('errorCode'), 0)
