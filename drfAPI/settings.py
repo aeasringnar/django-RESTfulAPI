bd574622d4532ae5e3e612d01a7b2a18b09e5ff8
aeas-tencet-server <aeasringnar@163.com>
2022-08-29 21:25:27 +0800

    add custom command startmyapp

--- drfAPI/settings.py
+++ drfAPI/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+MY_APP_TEMPLATE = Path.joinpath(BASE_DIR, 'my_app_template')
+MY_APPS_DIR = Path.joinpath(BASE_DIR, 'apps')
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'startmyapp',
     'rest_framework'
 ]
 
---
+++ startmyapp/management/commands/startmyapp.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+from os import system, makedirs
+from pathlib import Path
+
+class Command(BaseCommand):
+    help = "Command for creating a custom app."
+
+    def add_arguments(self, parser):
+        '''为命令添加一个参数，就是要创建app的名称，用来调用内置的startapp的必传参数。'''
+        parser.add_argument('app_name')
+
+    def handle(self, *args, **options):
+        '''实际是业务逻辑，根据自己的需要，变更原来的创建新的app的逻辑。'''
+        app_name = options['app_name']
+        app_path = Path(Path.joinpath(settings.MY_APPS_DIR, app_name))
+        if app_path.exists():
+            raise CommandError("This app(%s) is exists." % app_name)
+        makedirs(app_path)
+        cmd = "python manage.py startapp --template={} {} {}".format(settings.MY_APP_TEMPLATE, app_name, app_path)
+        # 执行实际的创建app的命令
+        system(cmd)
