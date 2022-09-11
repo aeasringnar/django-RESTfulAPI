8119beff0419772cea5e039fa89234e705187c27
aeas-tencet-server <aeasringnar@163.com>
2022-09-11 17:17:33 +0800

    use custom field

--- extensions/BaseModel.py
+++ extensions/BaseModel.py
@@ -1,10 +1,11 @@
-import time
 # from typing import *
 from datetime import datetime
 from django.db.models import Model, Manager
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models, transaction
+from soft_delete_new.models import SoftDeleteModel
 from utils.MyDateTime import MyDateTime
+from extensions.MyFields import TimestampField
 
 
 class BigDataFilterManager(Manager):
@@ -22,30 +23,29 @@ class BigDataFilterManager(Manager):
 class BaseModel(Model):
     # sort = models.IntegerField(default=1, verbose_name='排序')
     remark = models.TextField(max_length=1024, default='', blank=True, verbose_name='备注')
-    sort_timestamp = models.BigIntegerField(validators=[MinValueValidator(limit_value=0)], verbose_name='排序时间戳，默认等于创建时间戳')
-    create_timestamp = models.BigIntegerField(validators=[MinValueValidator(limit_value=0)], verbose_name='创建时间戳')
-    update_timestamp = models.BigIntegerField(validators=[MinValueValidator(limit_value=0)], verbose_name='更新时间戳')
+    sort_timestamp = TimestampField(auto_now_add=True, validators=[MinValueValidator(limit_value=0)], verbose_name='排序时间戳，默认等于创建时间戳')
+    create_timestamp = TimestampField(auto_now_add=True, validators=[MinValueValidator(limit_value=0)], verbose_name='创建时间戳')
+    update_timestamp = TimestampField(auto_now=True,validators=[MinValueValidator(limit_value=0)], verbose_name='更新时间戳')
     # objects = BigDataFilterManager()  # 是否开放大数据时的日期过滤
 
     class Meta:
         abstract = True
     
-    @transaction.atomic
-    def save(self, *args, **kwargs) -> None:
-        now_time = int(time.time() * 1e6)
-        # print(dir(self))
-        if self.pk is None:
-            # print("数据正则创建")
-            self.create_timestamp = now_time
-            self.sort_timestamp = now_time
-        else:
-            pass
-            # print("数据正则修改")
-        self.update_timestamp = now_time
-        super().save(*args, **kwargs)
+    # @transaction.atomic
+    # def save(self, *args, **kwargs) -> None:
+    #     now_time = int(time.time() * 1e6)
+    #     # print(dir(self))
+    #     if self.pk is None:
+    #         # print("数据正则创建")
+    #         self.create_timestamp = now_time
+    #         self.sort_timestamp = now_time
+    #     else:
+    #         pass
+    #         # print("数据正则修改")
+    #     self.update_timestamp = now_time
+    #     super().save(*args, **kwargs)
     
     @transaction.atomic
     def delete(self, *args, **kwargs) -> None:
-        now_time = int(time.time() * 1e6)
-        self.update_time = now_time
+        self.update_timestamp = MyDateTime.datetime_timestamp(datetime.now())
         super().delete(*args, **kwargs)
---
+++ extensions/MyFields.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from django.db.models import BigIntegerField
+from utils.MyDateTime import MyDateTime
+
+
+class TimestampField(BigIntegerField):
+    
+    description = "Custom timestamp field."
+    
+    def __init__(self, verbose_name=None, name=None, auto_now=False,
+                 auto_now_add=False, **kwargs):
+        self.auto_now, self.auto_now_add = auto_now, auto_now_add
+        if auto_now or auto_now_add:
+            kwargs['editable'] = False
+            kwargs['blank'] = True
+        super().__init__(verbose_name, name, **kwargs)
+    
+    def pre_save(self, model_instance, add):
+        if self.auto_now or (self.auto_now_add and add):
+            value = MyDateTime.datetime_timestamp(datetime.now())
+            setattr(model_instance, self.attname, value)
+            return value
+        else:
+            return super().pre_save(model_instance, add)
