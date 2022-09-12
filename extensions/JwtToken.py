59f351d58181201edcf6d56843a9952c32b6565e
aeas-tencet-server <aeasringnar@163.com>
2022-09-12 15:40:13 +0800

    update jwt auth jwt token obj

--- extensions/JwtToken.py
+++ extensions/JwtToken.py
@@ -3,7 +3,7 @@ import logging
 from typing import *
 from datetime import datetime
 from django.conf import settings
-from jwt.exceptions import ExpiredSignatureError, InvalidSignatureError
+from jwt.exceptions import ExpiredSignatureError, InvalidSignatureError, DecodeError
 from django.db.models import Model
 
 
@@ -26,6 +26,7 @@ class JwtToken:
         # self.leeway = settings.JWT_SETTINGS['LEEWAY']
     
     def encode(self, payload: dict) -> str:
+        '''将目标信息转为jwt的值'''
         tmp = {
             'exp': datetime.utcnow() + settings.JWT_SETTINGS['ACCESS_TOKEN_LIFETIME']
         }
@@ -33,6 +34,7 @@ class JwtToken:
         return jwt.encode(payload=payload, key=self.key, algorithm=self.algrithm)
     
     def decode(self, s: str) -> tuple:
+        '''将jwt的值，也就是token转为目标对象'''
         try:
             res = jwt.decode(
                 jwt=s,
@@ -45,15 +47,19 @@ class JwtToken:
             return None, 'Token expired.'
         except InvalidSignatureError as e:
             return None, 'Token is not valid.'
+        except DecodeError as e:
+            return None, 'Not enough segments.'
     
     def encode_user(self, payload: dict) -> str:
-        if isinstance(payload, dict):
+        '''将输入的payload，主要是要加密的数据，转为jwt的token字符串'''
+        if not isinstance(payload, dict):
             raise ValueError("Payload must be a dict type.")
         if 'id' not in payload or 'jwt_version' not in payload:
             raise KeyError("Payload must contain the 'id' and 'jwt_ersion' fields.")
         return self.encode(payload)
     
     def decode_user(self, s: str, User: Model) -> tuple:
+        '''将传入的jwt的token值转为内置的User对象'''
         try:
             obj, msg = self.decode(s)
             if not obj: return obj, msg
@@ -70,7 +76,7 @@ class JwtToken:
     def check_headers_jwt(self, target: str) -> tuple:
         '''检查请求头中的jwt'''
         target_ls = target.strip().split(" ")
-        if len(target_ls) != 2: return None, "Invalid Authorization header. No credentials provided."
+        if len(target_ls) != 2: return None, "Invalid Authorization header. No credentials provided. Need 'Bearer <token value>'."
         header_type, value = target_ls
-        if header_type != self.header_type: return None, "The message header is invalid."
+        if header_type != self.header_type: return None, "The message header is invalid, need 'Bearer <token value>'."
         return value, ""
