6658d0ad731edd9221236355f93a150fa9a60ad8
aeas-tencet-server <aeasringnar@163.com>
2022-09-18 14:51:18 +0800

    update app tempalte

---
+++ apps/user/tasks.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import json
+import time
+import logging
+import threading
+from decimal import Decimal
+from datetime import datetime, timedelta
+from django.conf import settings
+from django.db import transaction
+from django.core.cache import caches
+from django.forms.models import model_to_dict
+from celery import shared_task
+from drfAPI.celery import app
+
+
+# @app.task(bind=True)
+# def async_task(self, *args, **kwargs):
+#     try:
+#         pass
+#     except Exception as e:
+#         logging.error("async task has error {}".fromat(e))
+#         logging.exception(e)
+#         # 执行失败重试，本例设置3分钟后重试
+#         self.retry(countdown=60 * 3, exc=e)
