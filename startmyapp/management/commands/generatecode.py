7567119f32431eb79a79305f6e4094b4540b8850
aeas-tencet-server <aeasringnar@163.com>
2022-09-19 22:07:27 +0800

    update generate code for view

--- startmyapp/management/commands/generatecode.py
+++ startmyapp/management/commands/generatecode.py
@@ -3,14 +3,12 @@ from django.conf import settings
 from os import system, makedirs
 from pathlib import Path
 import json
+import logging
+import sys
 
 
 class Command(BaseCommand):
     help = "Command for generate template code."
-
-    # def add_arguments(self, parser):
-    #     '''为命令添加一个参数，就是要创建app的名称，用来调用内置的startapp的必传参数。'''
-    #     parser.add_argument('app_name')
     now_dir = Path(__file__).resolve().parent.parent.parent
     generate_template_dir = Path.joinpath(now_dir, 'generate_template')
     base_str_path_dic = {
@@ -22,6 +20,12 @@ class Command(BaseCommand):
     def get_base_str(self, str_type: str) -> str:
         with open(self.base_str_path_dic[str_type], 'r') as f:
             return f.read()
+    
+    def generate_item_code(self, app_path: str, file_name: str, content: str) -> bool:
+        '''生成文件的方法'''
+        with open(Path.joinpath(app_path, file_name), 'a') as f:
+            f.write(content)
+        return True
 
     def handle(self, *args, **options):
         json_path = Path.joinpath(settings.CONFIG_DIR, 'generateCode.json')
@@ -33,16 +37,31 @@ class Command(BaseCommand):
             if not app_path.exists():
                 raise CommandError("This app(%s) is not exists." % app_name)
             # 开启生成代码的流程
-            print('app：', data)
+            print('即将开始生成代码的App名称：{}'.format(app_name))
             models = data.get('models')
-            print('所有模型：', models)
             w_serializer = []
-            base_ser = self.get_base_str('serializer')
+            w_view = []
             for model_item in models:
                 model_name = model_item.get('model_name')
                 verbose = model_item.get('verbose')
-                searchs = model_item.get('searchs')
-                filters = model_item.get('filters')
+                base_ser = self.get_base_str('serializer')
                 w_serializer.append(base_ser.format(model_name=model_name, verbose=verbose))
-            with open(Path.joinpath(app_path, 'serializers.py'), 'a') as f:
-                f.write('\n\n\n'.join(w_serializer))
+                if model_item.get('searchs'):
+                    searchs = ["'{}'".format(item) for item in model_item.get('searchs')]
+                    search_str = ", ".join(searchs) if len(searchs) > 1 else "%s, " % searchs[0]
+                    search_str = '''\n    search_fields = (%s)''' % search_str
+                else:
+                    search_str = ''
+                if model_item.get('filters'):
+                    filters = ["'{}'".format(item) for item in model_item.get('filters')]
+                    filter_str = ", ".join(filters) if len(filters) > 1 else "%s, " % filters[0]
+                    filter_str = '''\n    filter_fields = (%s)''' % filter_str
+                else:
+                    filter_str = ''
+                base_view = self.get_base_str('view')
+                w_view.append(base_view.format(verbose=verbose, model_name=model_name, search_str=search_str, filter_str=filter_str))
+            # 生成序列化器文件
+            # self.generate_item_code(app_path, 'serializers.py', '\n\n\n'.join(w_serializer))
+            print("App：{} 的序列化器文件生成完毕。".format(app_name))
+            self.generate_item_code(app_path, 'views.py', '\n\n\n'.join(w_view))
+            print("App：{} 的视图文件生成完毕。".format(app_name))
