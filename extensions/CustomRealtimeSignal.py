c8df7f25083f614e751ac0b35a512484cf1c9283
aeas-tencet-server <aeasringnar@163.com>
2022-09-27 23:18:16 +0800

    add custom realtime signal

---
+++ extensions/CustomRealtimeSignal.py
@@ -0,0 +1,54 @@
+from django.db import models
+from haystack.exceptions import NotHandled
+from haystack.signals import BaseSignalProcessor
+
+
+class RealtimeSignalProcessor(BaseSignalProcessor):
+    """
+    Allows for observing when saves/deletes fire & automatically updates the
+    search engine appropriately.
+    """
+    
+    def handle_save(self, sender, instance, **kwargs):
+        """
+        Given an individual model instance, determine which backends the
+        update should be sent to & update the object on those backends.
+        """
+        using_backends = self.connection_router.for_write(instance=instance)
+        # 只在发现是创建数据的时候才更新索引。原因是不知道什么导致，model在保存是更新索引大致大量的重复索引。
+        if not kwargs.get('created', False):
+            return
+        for using in using_backends:
+            try:
+                index = self.connections[using].get_unified_index().get_index(sender)
+                index.update_object(instance, using=using)
+            except NotHandled:
+                # TODO: Maybe log it or let the exception bubble?
+                pass
+    
+    def handle_delete(self, sender, instance, **kwargs):
+        """
+        Given an individual model instance, determine which backends the
+        delete should be sent to & delete the object on those backends.
+        """
+        using_backends = self.connection_router.for_write(instance=instance)
+
+        for using in using_backends:
+            try:
+                index = self.connections[using].get_unified_index().get_index(sender)
+                index.remove_object(instance, using=using)
+            except NotHandled:
+                # TODO: Maybe log it or let the exception bubble?
+                pass
+
+    def setup(self):
+        # Naive (listen to all model saves).
+        models.signals.post_save.connect(self.handle_save)
+        models.signals.post_delete.connect(self.handle_delete)
+        # Efficient would be going through all backends & collecting all models
+        # being used, then hooking up signals only for those.
+
+    def teardown(self):
+        # Naive (listen to all model saves).
+        models.signals.post_save.disconnect(self.handle_save)
+        models.signals.post_delete.disconnect(self.handle_delete)
