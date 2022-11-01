1a83c6513bddd60f57fc02ec4f04b7aba8fd23c0
aeas-tencet-server <aeasringnar@163.com>
2022-11-01 09:34:09 +0800

    test

--- apps/user/models.py
+++ apps/user/models.py
@@ -26,6 +26,7 @@ class User(BaseModel):
         male = 'male', gettext_lazy('男')
         privacy = 'privacy', gettext_lazy('保密')
         
+        
     group = models.ForeignKey(Group, on_delete=models.PROTECT, null=True, verbose_name='用户组')
     username = models.CharField(max_length=32, default='', blank=True, verbose_name='用户账号')
     password = models.CharField(max_length=255, default='',blank=True, verbose_name='用户密码')
