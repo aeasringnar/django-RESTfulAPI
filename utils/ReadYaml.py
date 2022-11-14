e756f520a6d5ed1312a3fa8375df26d6d26802dc
aeas-tencet-server <aeasringnar@163.com>
2022-11-14 23:04:39 +0800

    update

--- utils/ReadYaml.py
+++ utils/ReadYaml.py
@@ -6,7 +6,7 @@ from django.conf import settings
 class ReadYaml:
     '''用于读取yaml文件的类'''
     
-    def __init__(self, file_name, is_path=False):
+    def __init__(self, file_name: str, is_path: bool=False) -> None:
         self.filepath = file_name if is_path else Path.joinpath(settings.CONFIG_DIR, file_name)
 
     def get_data(self):
